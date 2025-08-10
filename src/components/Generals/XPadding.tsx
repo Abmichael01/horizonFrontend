5c4252d7ee35bed5d419cbb2e0442b0a5b6a7245
Urkel <abmichael109@gmail.com>
2025-08-10 02:17:14 +0100

    new changes

--- src/components/Generals/XPadding.tsx
+++ src/components/Generals/XPadding.tsx
@@ -9,9 +9,9 @@ type XPaddingProps = BoxProps & { // Use BoxProps here to type the props correct
 export default function XPadding({ children, className, ...rest }: XPaddingProps) {
   return (
     <Box
-      px={[2, 5, 10, 20]} // Default padding values from Chakra UI's `Box`
+      px={[4, 5, 10, 20]} // Default padding values from Chakra UI's `Box`
       className={cn(
-        "px-4 sm:px-6 lg:px-10", // Tailwind classes for padding
+        "px-5 sm:px-6 lg:px-10", // Tailwind classes for padding
         className // Allows custom classes to override or extend
       )}
       {...rest} // Spread the rest of the props here (like `style`, `onClick`, etc.)
